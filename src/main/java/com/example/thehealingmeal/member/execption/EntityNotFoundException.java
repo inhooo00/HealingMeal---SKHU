1544cde6aee2698c478725252c4eab8dc82cd151
inhooo00 <chldlsgh0987@naver.com>
2024-03-30 11:55:23 +0900

    [feat] EntityNotFoundException 추가

--- src/main/java/com/example/thehealingmeal/member/execption/EntityNotFoundException.java
+++ src/main/java/com/example/thehealingmeal/member/execption/EntityNotFoundException.java
@@ -1,2 +1,20 @@
-package com.example.thehealingmeal.member.execption;public class EntityNotFoundException {
+package com.example.thehealingmeal.member.execption;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    private final Exception hiddenException;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+        this.hiddenException = null;
+    }
+
+    public EntityNotFoundException(String entityName, Object keyValue, Exception exception) {
+        super(entityName + " with id " + keyValue + " not found. Additional info: " + exception.getMessage());
+        this.hiddenException = exception;
+    }
+
+    public Exception getHiddenException() {
+        return hiddenException;
+    }
 }
